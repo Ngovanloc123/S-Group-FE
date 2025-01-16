614cb4bd48f07678ceb8b26d72313efab4e19aa0
NgoVanLoc <62205ngovanloc@gmail.com>
2025-01-16 19:54:03 +0700

    Submit BT JS

--- Learn JS/B1/fn.js
+++ Learn JS/B1/fn.js
@@ -1,3 +1,43 @@
+//--------- BT1 -----------------------------------------
+
+const date = new Date();
+
+// Tháng bắt đầu từ 0
+// Thứ trong tuần bắt đầu từ 1 (thứ hai => 1)
+
+let mm = date.getMonth() + 1; // Months start at 0!
+let dd = date.getDate();
+
+// Định dạng lại ngày và tháng
+if (dd < 10) dd = "0" + dd;
+if (mm < 10) mm = "0" + mm;
+
+console.log("Bài tập 1:");
+console.log(
+  date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds() +
+    " " +
+    dd +
+    "/" +
+    mm +
+    "/" +
+    date.getFullYear()
+);
+
+//--------- BT2 -----------------------------------------
+
+console.log("\nBài tập 2:");
+console.log("Định dạng mm-dd-yyyy");
+
+console.log(mm + "-" + dd + "-" + date.getFullYear());
+console.log("Định dạng mm/dd/yyyy");
+console.log(mm + "/" + dd + "/" + date.getFullYear());
+console.log("Định dạng dd-mm-yyyy");
+console.log(dd + "-" + mm + "-" + date.getFullYear());
+
 //--------- BT3 -----------------------------------------
 function isNumberIncrease(number) {
   // Chuyển số thành string
@@ -53,6 +93,35 @@ function isContainJava(str) {
 console.log("Bài tập 7:");
 console.log(isContainJava("JavaScript"));
 console.log(isContainJava("javaScript"));
+
+//--------- BT8 -----------------------------------------
+
+function getMonthForNumber(number) {
+  // Ngoại lệ
+  if (number <= 0 || number > 12) {
+    return "Tháng không hợp lệ!!!";
+  }
+  // Mảng chứa tên các tháng
+  const months = [
+    "Tháng một",
+    "Tháng hai",
+    "Tháng ba",
+    "Tháng bốn",
+    "Tháng năm",
+    "Tháng sáu",
+    "Tháng bảy",
+    "Tháng tám",
+    "Tháng chín",
+    "Tháng mười",
+    "Tháng mười một",
+    "Tháng mười hai",
+  ];
+  return months[number - 1];
+}
+
+console.log("\nBài tập 8:");
+let month = 12;
+console.log(getMonthForNumber(month));
 
 //--------- BT9 -----------------------------------------
 function substrMax(str) {
